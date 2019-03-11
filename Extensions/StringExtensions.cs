1bec13216e21805c8e599ea284fcdda4c4ada238
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-03-11 18:13:20 +0545

    initial commit

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fuzzy_search_Demo.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsFuzzySimilar(this string input, string parameter, int threshold = 3)
+        {
+            if (string.IsNullOrEmpty(parameter))
+            {
+                throw new ArgumentNullException($"parameter can't be empty or null");
+            }
+
+            if (threshold < 0)
+            {
+                throw new InvalidOperationException($"threshold can't be less than 0");
+            }
+
+            return (LevenshteinDistance.Compute(input, parameter) < threshold) || input.Contains(parameter) || parameter.Contains(input);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Fuzzy_search_Demo.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fuzzy_search_Demo
+{
+    class Program
+	{
+        static void Main(string[] args)
+        {
+            List<string> l = new List<string>
+            {
+                "ant",
+                "aunt",
+                "Sam",
+                "Samantha" ,
+                "clozapine",
+                "olanzapine" ,
+                "annt",
+                "volmax" ,
+                "toradol",
+                "at" ,
+                "kitten",
+                "sitting" 
+            };
+
+            Console.WriteLine("\n\n\n---------------------------------------------------------------");
+            Console.WriteLine("All Items in List");
+            foreach (string a in l)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("---------------------------------------------------------------");
+
+            Console.WriteLine("\n\n\nEnter Search Term to search on the list");
+            var searchTerm = Console.ReadLine();
+             
+            var searchedString = l.Where(x => searchTerm.IsFuzzySimilar(x, 2));
+
+
+            Console.WriteLine("\n\n\n---------------------------------------------------------------");
+            Console.WriteLine("Fuzzy search result is : ");
+            foreach (var item in searchedString)
+            {
+                Console.WriteLine(item); 
+            }
+
+            Console.WriteLine("\n\n\n==========Enter any key to exit============");
+            Console.ReadKey();   
+        }
+
+	} 
+}
